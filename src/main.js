6f90eaac1b5d0ad3cad81f765aae7ab72e1e3f69
linxi <linxi@1242838858@qq.com>
2019-09-20 01:00:31 +0800

    登录页完成(校验及数据规则)

--- src/main.js
+++ src/main.js
@@ -3,6 +3,21 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import './style/index.less'
+// 引入表单校验组件
+import {
+  ValidationProvider, // 只能验证一个字段
+  ValidationObserver, // 能验证多个字段
+  extend
+} from 'vee-validate' // 没有携带内置的验证规则
+// import {
+//   ValidationProvider,
+//   // ValidationObserver,
+//   extend
+// } from 'vee-validate/dist/vee-validate.full' // 携带内置的验证规则
+// 加载验证插件的语言包
+import zhCN from 'vee-validate/dist/locale/zh_CN' // 中文
+import * as rules from 'vee-validate/dist/rules' // 所有的验证规则
+
 // 按需引入 Vant 组件
 import {
   Button, // 按钮
@@ -14,7 +29,32 @@ import {
 } from 'vant'
 
 Vue.use(NavBar).use(Button).use(Field).use(Icon).use(Toast).use(Loading)
+// 配置表单验证插件
+for (let rule in rules) {
+  extend(rule, {
+    ...rules[rule], // add the rule
+    message: zhCN.messages[rule] // add its message
+  })
+}
 
+// 配置自定义手机号验证
+extend('phone', {
+  // value会收到文本框的改变的数据
+  // validate: value => /^1\d{10}$/.test(value),
+  validate (value) {
+    return /^1\d{10}$/.test(value)
+  },
+  message: '必须是手机号'
+})
+// 自定义验证规则
+// extend('secret', {
+//   // value会收到文本框的改变的数据
+//   validate: value => value === 'example',
+//   message: 'This is not the magic word'
+// });
+// 注册为全局组件
+Vue.component('ValidationProvider', ValidationProvider)
+Vue.component('ValidationObserver', ValidationObserver)
 Vue.config.productionTip = false
 
 new Vue({
--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -2,13 +2,54 @@
 <template>
   <div class="login">
     <van-nav-bar title="登录" />
+
+    <!-- ValidationProvider 是验证插件提供的一个全局组件
+            rules="select" 配置验证规则,当文本框发生改变的时候自动调用select校验规则
+            v-slot="{errors}" 获取验证相关的结果参数
+            errors[0]用来获取验证失败的错误消息
+
+    -->
+    <!--练习  ValidationProvider只能验证一个字段
+        <ValidationProvider rules="required|email" v-slot="{ errors }">
+      <input v-model="user.mobile" type="text" />
+      <span>{{ errors[0] }}</span>
+    </ValidationProvider>-->
+    <!-- ValidationObserver 组件会渲染成一个form表单
+        可以通过ref 调用这个组件的方法:validate来进行验证
+    -->
     <van-cell-group>
-      <van-field v-model="user.mobile" required clearable label="手机号" placeholder="请输入手机号" />
-      <van-field v-model="user.code" required type="password" label="验证码" placeholder="请输入验证码" />
-      <div class="login-btn">
-        <van-button :loading="isLoading" type="info" @click="onLogin">登录</van-button>
-      </div>
+      <ValidationObserver tag="form" ref="loginForm">
+        <!-- 把需要验证的字段使用 ValidationProvider包裹起来
+                在其上面配置对应的验证规则等信息
+        -->
+        <ValidationProvider tag="div" rules="required|phone" name="手机号" v-slot="{ errors }">
+          <!-- van-field提供了提示错误信息 :error-message="errors[0]" -->
+          <van-field
+            v-model="user.mobile"
+            required
+            clearable
+            label="手机号"
+            placeholder="请输入手机号"
+            :error-message="errors[0]"
+          />
+        </ValidationProvider>
+
+        <ValidationProvider tag="div" rules="required" name="验证码" v-slot="{ errors }">
+          <van-field
+            v-model="user.code"
+            required
+            type="password"
+            label="验证码"
+            placeholder="请输入验证码"
+            :error-message="errors[0]"
+          />
+        </ValidationProvider>
+      </ValidationObserver>
     </van-cell-group>
+
+    <div class="login-btn">
+      <van-button :loading="isLoading" type="info" @click="onLogin">登录</van-button>
+    </div>
   </div>
 </template>
 <script>
@@ -29,7 +70,6 @@ export default {
   },
   methods: {
     async onLogin () {
-      this.isLoading = true
       //   try {
       //     const { data } = await request({
       //       method: "POST",
@@ -41,6 +81,14 @@ export default {
       //       this.$toast.fail("手机号或验证码错误");
       //     }
       try {
+        //   表单验证
+        const isValid = await this.$refs.loginForm.validate()
+        if (!isValid) {
+          // 如果验证不通过直接停止登录
+          return
+        }
+        // 提交表单验证通过
+        this.isLoading = true
         const { data } = await login(this.user)
         // console.log(data);
         // 成功的提示
